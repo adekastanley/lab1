b30e538b61798478ca1ed565d738c7fc93f79873
adeka stanley <adekastanley2@gmail.com>
2023-03-26 17:02:40 +0100

    added gsap animations for nav bar

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 
 const hamburger = document.querySelector('.ham-container')
-
+let navOPen = false;
 hamburger.addEventListener('click', ()=> {
+    if (navOPen){
+        gsap.timeline()
+        .to('.nav-container',{
+           x: -200, ease:"power2.out" 
+        })
+        navOPen = false
+    } else {
+        gsap.timeline()
+        .to('.nav-container',{
+           x: 200, ease:"power2.out" 
+        })
+        navOPen = true
+    }
 
 })
